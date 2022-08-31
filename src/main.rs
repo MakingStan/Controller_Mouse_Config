61302ecffb9fcb9fed17b5221d7f820894cad4b7
MakingStan <78844475+MakingStan@users.noreply.github.com>
2022-08-31 18:29:34 +0200

    removed .idea directory

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
+use egui::TextBuffer;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
-use egui::{TextBuffer, Vec2};
 
-fn make_controller_mouse_directory_and_config_file(mut controller_mouse_path: String)
-{
+fn make_controller_mouse_directory_and_config_file(controller_mouse_path: String) {
     let dir_path = Path::new(&controller_mouse_path);
 
     //check if the directory even exists, if not make it.
-    if !dir_path.is_dir()
-    {
+    if !dir_path.is_dir() {
         fs::create_dir(&dir_path).unwrap();
     }
 
-    let config_file_path: String = controller_mouse_path+"/config.txt";
+    let config_file_path: String = controller_mouse_path + "/config.txt";
 
     match File::open(config_file_path.clone()) {
-        Ok(ref _file) => { },
-        Err(error) => {
+        Ok(ref _file) => {}
+        Err(_error) => {
             //the config file doesn't exist make it.
             File::create(config_file_path.clone()).expect("Could not create config file.");
-            fs::write(config_file_path.clone(), "20").expect("Could not write initial config file settings.");
-        },
+            fs::write(config_file_path.clone(), "20")
+                .expect("Could not write initial config file settings.");
+        }
     };
 }
 
@@ -41,25 +40,21 @@ impl Default for ControllerMouseConfig {
     }
 }
 
-fn get_config(index: usize) -> String
-{
-
-    return fs::read_to_string(home::home_dir().unwrap().to_str().unwrap().to_owned() + "/.controller_mouse/config.txt")
-        .unwrap()
-        .as_str()
-        .split("\n")
-        .collect::<Vec<&str>>()
-        .get(index)
-        .unwrap()
-        .to_string();
+fn get_config(index: usize) -> String {
+    return fs::read_to_string(
+        home::home_dir().unwrap().to_str().unwrap().to_owned() + "/.controller_mouse/config.txt",
+    )
+    .unwrap()
+    .as_str()
+    .split("\n")
+    .collect::<Vec<&str>>()
+    .get(index)
+    .unwrap()
+    .to_string();
 }
 
-
 impl eframe::App for ControllerMouseConfig {
-
-
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Mouse Controller Config");
             ui.add(egui::Slider::new(&mut self.sensivity, 2..=100).text("sensivity"));
@@ -69,22 +64,33 @@ impl eframe::App for ControllerMouseConfig {
             ui.heading("Vid (Vendor id)");
             ui.text_edit_singleline(&mut self.vid);
 
-
-            ui.heading("\nIf you don't know your vid or/and pid follow the guide at the github page: ");
-            ui.text_edit_singleline(&mut "https://github.com/makingstan/controller_mouse/blob/master/README.md");
-
+            ui.heading(
+                "\nIf you don't know your vid or/and pid follow the guide at the github page: ",
+            );
+            ui.text_edit_singleline(
+                &mut "https://github.com/makingstan/controller_mouse/blob/master/README.md",
+            );
 
             if ui.button("Update Data").clicked() {
-                fs::write(home::home_dir().unwrap().to_str().unwrap().to_owned() + "/.controller_mouse/config.txt", (102-self.sensivity).to_string()+"\n".as_str()+self.vid.as_str()+"\n".as_str()+self.pid.as_str()).expect("Could not write to config.txt");
+                fs::write(
+                    home::home_dir().unwrap().to_str().unwrap().to_owned()
+                        + "/.controller_mouse/config.txt",
+                    (102 - self.sensivity).to_string()
+                        + "\n".as_str()
+                        + self.vid.as_str()
+                        + "\n".as_str()
+                        + self.pid.as_str(),
+                )
+                .expect("Could not write to config.txt");
             }
-
         });
     }
 }
 
-
 fn main() {
-    make_controller_mouse_directory_and_config_file(home::home_dir().unwrap().to_str().unwrap().to_owned() + "/.controller_mouse");
+    make_controller_mouse_directory_and_config_file(
+        home::home_dir().unwrap().to_str().unwrap().to_owned() + "/.controller_mouse",
+    );
     let options = eframe::NativeOptions::default();
 
     eframe::run_native(
@@ -92,5 +98,4 @@ fn main() {
         options,
         Box::new(|_cc| Box::new(ControllerMouseConfig::default())),
     );
-
 }
